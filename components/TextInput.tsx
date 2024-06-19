a37af0819d7abfb4d45222704080f6faf27de943
Michael Cox <29073478+cox-michael@users.noreply.github.com>
2024-06-19 04:15:00 +0000

    Create input field

---
+++ components/TextInput.tsx
@@ -0,0 +1,13 @@
+import { TextInput } from "react-native";
+import styled from "styled-components";
+
+export const RetroTextInput = styled(TextInput)`
+  font-size: 14px;
+  color: #00ff00;
+  background-color: black;
+  border: 2px solid #00ff00;
+  padding: 10px;
+  width: 80%;
+  margin: 10px auto;
+  text-align: left;
+`;
